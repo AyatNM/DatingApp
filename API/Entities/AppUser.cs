88d8d39c0eae05822d8c5ab8443ee0c4cf2643f4
Ayat <ayatm.6c@gmail.com>
2021-04-22 16:38:31 +0300

    add user and ssl

--- API/Entities/AppUser.cs
+++ API/Entities/AppUser.cs
@@ -2,13 +2,15 @@ namespace API.Entities
 {
     public class AppUser
     {
+        public AppUser(int id, string userName)
+        {
+            this.Id = id;
+            this.UserName = userName;
+
+        }
         public int Id { get; set; }
         public string UserName { get; set; }
-
         public byte[] PasswordHash { get; set; }
-
         public byte[] PasswordSalt { get; set; }
-      
-        
     }
 }
--- API/Startup.cs
+++ API/Startup.cs
@@ -4,6 +4,7 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
 
 namespace API
 {
@@ -27,23 +28,21 @@ namespace API
 
         }
 
-
-
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
-            // if (env.IsDevelopment())
-            // {
-            //     app.UseDeveloperExceptionPage();
-            //     app.UseSwagger();
-            //     app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "API v1"));
-            // }
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+                //app.UseSwagger();
+                //app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "API v1"));
+            }
 
-            // app.UseHttpsRedirection();
+            app.UseHttpsRedirection();
 
             app.UseRouting();
 
-            app.UseCors(x => x.AllowAnyHeader().AllowAnyMethod().WithOrigins("http://localhost:4200"));
+            app.UseCors(x => x.AllowAnyHeader().AllowAnyMethod().WithOrigins("https://localhost:4200"));
 
             app.UseAuthorization();
 
